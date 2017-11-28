1c92c918846e5556e0f99f2c71a0cb052762d4e4
gordydwyer <gdwyer@LAPTOP-M6DCN7B6.localdomain>
2017-11-27 22:13:44 -0500

    began coding for overall inventory handling

--- fridge.c
+++ fridge.c
@@ -1,46 +1,35 @@
-// This file will be the main loop that monitors serial comms
-// for updates to presence of objects, as well as updates to their weights
-// and control the inventory process (motor and camera)
 #include "stdio.h"
 #include "struct.h"
 
-char temp[100];
+// Index 0 = location 0 etc. index does not correlate to anything in the outFridge array
+item_t inFridge[4];	//array of items currently in the fridge
+item_t outFridge[4];	//array of items that have been removed from the fridge and are awaiting re-entry
 
-int main()
+
+void take_picture()
 {
-	item test;
-	//char * name_ptr = test.name[0];
-	//char * conf_ptr = test.confidence[0];
+	system("raspistill -o test.jpeg");
+	system("python takepic.py");
+	return;
+}
+
+
+// takes a picture using picam, and from the output of the python code, populates the name and confidence
+// variables of the proper location 
+void vision(int loc)
+{
+	take_picture();
 
 	FILE * file;
 	file = fopen("returncsv.csv", "r");
 	if (file)
 	{
 		// get first line of GVAPI output (best result)
-		fscanf(file, "%s", temp);
-		int i = 0;
-		while (temp[i] != ',')
-		{
-			test.name[i] = temp[i];
-			i++;
-		}	
-	
-		test.name[i] = '\0';
-		i++;	
-		int end = i + 4;
-		int j = 0;
-		for (i = i; i < end; i++)
-		{
-			test.confidence[j] = temp[i];
-			j++;
-		}
+		fscanf(file, "%s %s", inFridge[loc].name, inFridge[loc].confidence);
+		inFridge[loc].location = loc;	
 
-		test.confidence[4] = '\0';
-
-		fclose(file);
-
-		printf("%s\n", test.name);
-		printf("%s\n", test.confidence);
+		printf("%s\n", inFridge[loc].name);
+		printf("%s\n", inFridge[loc].confidence);
 	}
 
 	else
@@ -48,5 +37,9 @@ int main()
 		printf("ERROR OPENING");
 	}	
 
-	return 0;
+	return;
 }
+
+
+
+
--- struct.h
+++ struct.h
@@ -6,6 +6,8 @@
 	perent_left = percent of the item left based upon intial and current weight
 	location = which location on the lazy susan that the item is currently located at
 */	
+#include <string.h>
+
 typedef struct{
 	char 	name[24];
 	char	confidence[5];
@@ -13,11 +15,76 @@ typedef struct{
 	float	current_weight;
 	float 	percent_left;
 	int 	location;
-} item;
+} item_t;
 
-//function to set the percent_left memeber variable
-/*void set_percent(struct item)
+
+
+char* get_name(item_t item)
 {
-	item.percent_left = 100*(item.current_weight / item.intial_weight);
-	return 0;
-}*/
+	return item.name;
+}
+
+char* get_confidence(item_t item)
+{
+	return item.confidence;
+}
+
+float get_intital_weight(item_t item)
+{
+	return item.initial_weight;
+}
+
+float get_current_weight(item_t item)
+{
+	return item.current_weight;
+}
+
+float get_percent_left(item_t item)
+{
+	return item.percent_left;
+}
+
+int get_location(item_t item)
+{
+	return item.location;
+}
+
+void set_name(item_t item, char * name)
+{
+	strcpy(item.name, name);
+	//item.name = name;
+	return;
+}
+
+void set_confidence(item_t item, char * conf)
+{
+	strcpy(item.confidence, conf);
+	//item.confidence = conf;
+	return;
+}
+
+void set_initial_weight(item_t item, float weight)
+{
+	item.initial_weight = weight;
+	return;	
+}
+
+void set_current_weight(item_t item, float weight)
+{
+	item.current_weight = weight;
+	return;
+}
+
+void set_percent_left(item_t item)
+{
+	item.percent_left = 100*(item.current_weight / item.initial_weight);
+	return;
+}
+
+void set_location(item_t item, int loc)
+{
+	item.location = loc;
+	return;
+}
+
+
